18f0f1b00fa233af132a429effa6237e10fe7965
denys1953 <den25640@gmail.com>
2020-04-17 18:51:08 +0300

    master

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ var inp = document.getElementById('result');
 var par = document.getElementById('p');
 
 inp.addEventListener('blur', func);
-document.onclick = function() {
-   navigator.vibrate(200);
-}
 function func() {
    let time = inp.value;
 	let timer = setInterval(function() {
       if(time <= 0) {
          time = 0 + 1;
          clearInterval(timer);
-         navigator.vibrate(200);
+         navigator.vibrate(2000);
       }
       inp.onclick = function() {
          clearInterval(timer);
